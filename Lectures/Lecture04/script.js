9ff0e9d11d6efde3634453eaefce853b889adad4
James J. Boyer <aesthetics1@gmail.com>
2020-01-22 20:30:25 -0800

    Added operators to the lecture demo

--- Lectures/Lecture04/script.js
+++ Lectures/Lecture04/script.js
@@ -1,3 +1,5 @@
+// TYPES //
+
 // Whole number
 console.log(1);
 
@@ -23,3 +25,59 @@ console.log(null);
 
 // Undefined
 console.log(undefined);
+
+// OPERATORS //
+
+// Plus +
+console.log(5 + 10); // 15
+
+console.log("cat" + "dog"); // catdog
+console.log("cat" + " " + "dog"); // cat dog
+console.log("cat " + "dog"); // cat dog
+
+// Subtract - OR negative
+console.log(5 - 10); // -5
+console.log(-99); // -99
+
+// Multiplication *
+console.log(10 * 5); // 50
+console.log("five" * 2); // NaN
+
+// Division /
+console.log(50 / 10); // 5
+
+// Modulo %
+console.log(50 % 10); // 0
+console.log(5 % 2); // 1
+
+// HINT We can use modulo to determine whether a number is even or odd
+console.log(50 % 2); // IF this is 0, then 50 is even
+
+// typeof
+console.log(typeof 50); // number
+console.log(typeof "cat"); // string
+
+// Comparison operators
+
+// EQUAL TO
+console.log(true == true); // true
+
+// NOT EQUAL TO
+console.log(true != false); // true
+
+// GREATER THAN
+console.log(5 > 2); // true
+
+// LESS THAN
+console.log(5 < 2); // false
+
+// AND
+console.log(10 > 5 && 6 >= 2); // true
+
+// OR
+console.log(6 * 10 == 60 || 5 * 10 == 60); // true
+
+// TERNARY ?:
+// IF [statement] ? [do this] : [otherwise do this]
+
+console.log(75 >= 70 ? "PASS" : "FAIL"); // PASS
