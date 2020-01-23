e5738dfbf786de96ef0d5e5eeb4f8b1a9f8962d2
James J. Boyer <aesthetics1@gmail.com>
2020-01-22 19:37:53 -0800

    Added examples for types in lecture 4

---
+++ Lectures/Lecture04/script.js
@@ -0,0 +1,25 @@
+// Whole number
+console.log(1);
+
+// Another whole number
+console.log(36);
+
+// Fractional Number
+console.log(3.14);
+
+// Scientific Notation
+console.log(2.998e8);
+
+// String
+console.log("cat dog");
+
+// Boolean
+console.log(true);
+
+console.log(false);
+
+// Null
+console.log(null);
+
+// Undefined
+console.log(undefined);
