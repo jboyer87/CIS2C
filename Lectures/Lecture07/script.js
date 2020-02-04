d48338074f18736e18b910bcb538c2b4517d1083
James J. Boyer <aesthetics1@gmail.com>
2020-02-03 20:16:46 -0800

    Added part 2 of lecture demo

--- Lectures/Lecture07/script.js
+++ Lectures/Lecture07/script.js
@@ -47,3 +47,42 @@ console.log(square(9));
 const divideNumbers = (n, m) => n / m;
 
 console.log(divideNumbers(6, 3));
+
+// SCOPE
+
+let diameter = 10;
+
+if (diameter > 0) {
+  // Entering scope
+  let pi = 3.14;
+  let circumference = pi * diameter;
+  console.log("The circumference is: " + circumference); // 31.42
+} // Exiting scope
+// pi/circumference are not visible here, they are out of scope!
+
+console.log(diameter); // This works!
+// console.log(pi); // This doesn't work
+
+// NESTED SCOPE
+
+let myName = "Jamie";
+
+const greetMe = () => {
+  let myGreeting = "Hello, ";
+  console.log(myGreeting + myName + "!"); // My Name is visible
+};
+
+console.log(greetMe());
+// myGreeting is no longer visible, myName is still visible
+
+// Overflowing the Call stack
+
+const chicken = () => {
+  return egg();
+};
+
+const egg = () => {
+  return chicken();
+};
+
+console.log(chicken()); // ut oh... This is going to overflow!
