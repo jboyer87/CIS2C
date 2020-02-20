a626c26527bd3167fbee2e0bc5aacce6439868f7
James J. Boyer <aesthetics1@gmail.com>
2020-02-19 19:55:33 -0800

    Added lecture 12 part 2

--- Lectures/Lecture12/script.js
+++ Lectures/Lecture12/script.js
@@ -13,3 +13,41 @@ const addFifteen = number => {
 theResult = addFifteen(aNumber);
 
 console.log(theResult);
+
+// Exception Handling
+
+// creating a new error type called input error
+class InputError extends Error {}
+
+// mock function for prompting for a number, generates a random number
+const promptForNumber = () => {
+  return Math.floor(Math.random() * Math.floor(100));
+};
+
+// adds two numbers together if they are both numbers
+const addNumbers = (first, second) => {
+  if (isNaN(first) || isNaN(second)) {
+    throw new InputError("You may only enter numbers!");
+  }
+  return first + second;
+};
+
+while (true) {
+  let first = promptForNumber();
+  let second = promptForNumber();
+
+  //   let first = "This is definitely not a number";
+  //   let second = null;
+
+  try {
+    let result = addNumbers(first, second);
+    console.log(result);
+    break;
+  } catch (error) {
+    if (error instanceof InputError) {
+      console.log(error.message);
+    } else {
+      console.log("A generic error occurred");
+    }
+  }
+}
