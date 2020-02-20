f10b9e4f9bb93675c6e5d89d79313a091275b8f8
James J. Boyer <aesthetics1@gmail.com>
2020-02-19 20:07:11 -0800

    Added comments to lecture 12

--- Lectures/Lecture12/script.js
+++ Lectures/Lecture12/script.js
@@ -1,52 +1,71 @@
+// Part 1 - JavaScript's strict mode
+
 // "use strict"; <-- This would turn it on for the entire program
 
-aNumber = 15;
+aNumber = 15; // This would throw an error with "use strict" turned on!
 
 const addFifteen = number => {
-  "use strict";
+  "use strict"; // Enabling strict mode just for this function
 
   let fifteen = 15;
+  // fifteen = 15; This would throw an error with "use strict" turned on!
 
   return number + fifteen;
 };
 
+// Strict mode is not on anymore, we left the scope above where we enabled it!
 theResult = addFifteen(aNumber);
 
 console.log(theResult);
 
-// Exception Handling
+// Part 2 - Exception Handling
 
-// creating a new error type called input error
+// Creating a new error type called input error
+// You can create as many different types of errors as you want for different situations
 class InputError extends Error {}
 
-// mock function for prompting for a number, generates a random number
+// This is a mock function for prompting for a number, it just generates a random number
+// from 0-100
 const promptForNumber = () => {
   return Math.floor(Math.random() * Math.floor(100));
 };
 
-// adds two numbers together if they are both numbers
+// This function adds two numbers together if they are both numbers, otherwise it throws
+// an InputError with the message "You may only enter numbers!"
 const addNumbers = (first, second) => {
+  // If either first or second is not a number, throw an InputError
   if (isNaN(first) || isNaN(second)) {
     throw new InputError("You may only enter numbers!");
   }
+  // ...otherwise execution continues and the result is returned
   return first + second;
 };
 
+// This is an infinite loop that keeps trying to do something until the loop is broken
 while (true) {
+  // Prompt for 2 numbers (simulated user inputs)
   let first = promptForNumber();
   let second = promptForNumber();
 
+  // Uncomment these lines to see what happens when the code receives bad input
   //   let first = "This is definitely not a number";
   //   let second = null;
 
+  // Enter a try block - we are going to run code that might throw an error
   try {
     let result = addNumbers(first, second);
-    console.log(result);
-    break;
+    // If the above code doesn't return an error, these next two lines run
+    console.log(result); // Output the result
+    break; // Break out of the infinite loop
   } catch (error) {
+    // If an error occurs, catch it here
+    // Check to see if the error is an InputError (the error type that we defined)
     if (error instanceof InputError) {
+      // If it is, output the custom message we passed to it inside of addNumbers
       console.log(error.message);
     } else {
+      // Otherwise, just output a generic error (I don't know what kind of error this
+      // is)
       console.log("A generic error occurred");
     }
   }
